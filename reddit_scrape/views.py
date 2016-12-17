be52b04309d111baff6ca1000494daac6d9c4533
Jimmy <reilly@whitewavemedia.com>
2016-12-17 12:37:29 -0500

    saves reddit titles to db

--- reddit_scrape/views.py
+++ reddit_scrape/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from reddit_scrape.scrape import scrape_page
 
+from .models import Link
+
 def index(request):
 	return HttpResponse("this is just a test")
 
 def scrape_reddit(request):
 	titles = scrape_page(6)
-	return HttpResponse("saved")
 
+	insert_list = []
+	for link_title in titles:
+		insert_list.append(Link(title=link_title))
+
+	Link.objects.bulk_create(insert_list) 
+
+	return HttpResponse("saved")
